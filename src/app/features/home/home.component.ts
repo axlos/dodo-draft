0806e1ce72474d320a7246acffe93a9b67db2e4d
Edwin Torres <codesandtags@gmail.com>
2023-07-14 22:12:53 -0400

    chore: fixed bug and refactored call to action button

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,41 +1,36 @@
 import { Component } from '@angular/core';
-import * as AuthActions from "../../core/store/actions/auth.actions";
-import { Store } from "@ngrx/store";
-import { ViewportScroller } from "@angular/common";
-import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
-import { filter } from "rxjs/operators";
+import * as AuthActions from '../../core/store/actions/auth.actions';
+import { Store } from '@ngrx/store';
+import { ViewportScroller } from '@angular/common';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private viewportScroller: ViewportScroller,
-    private store: Store,
+    private store: Store
   ) {
-    this.router.events.pipe(
-      filter(event =>
-        event instanceof NavigationEnd
-      ),
-      filter(() =>
-        !!this.route.snapshot.fragment
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        filter(() => !!this.route.snapshot.fragment)
       )
-    ).subscribe(() => {
-      this.viewportScroller.scrollToAnchor(this.route.snapshot.fragment);
-    });
+      .subscribe(() => {
+        this.viewportScroller.scrollToAnchor(this.route.snapshot.fragment);
+      });
   }
 
   public signup(): void {
     this.store.dispatch(
       AuthActions.LoginActions.do({
-        screenHint: 'signup'
+        screenHint: 'signup',
       })
     );
   }
-
-
 }
