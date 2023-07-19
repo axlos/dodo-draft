22541e8ab019ebddc971979a82a4985fe29a6108
Alex Ospina <ingaxl@gmail.com>
2023-07-18 20:45:31 -0500

    cover letter examples

---
+++ src/app/features/home/components/cover-letter-examples/cover-letter-examples.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: 'app-cover-letter-examples',
+  templateUrl: './cover-letter-examples.component.html',
+  styleUrls: ['./cover-letter-examples.component.scss'],
+})
+export class CoverLetterExamplesComponent implements OnDestroy {
+
+  public examples: any[] = [];
+
+  private subscription = new Subscription();
+
+  constructor(
+    private translate: TranslateService
+  ) {
+    this.subscription.add(
+      this.translate.stream('home.cover-letter')
+        .subscribe((testimonial) => {
+          this.examples = [1, 2, 3].map((index) => {
+            const testI18n = testimonial[`example-${index}`];
+            return {
+              title: testI18n.title,
+              content: testI18n.content,
+            }
+          });
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,12 +3,15 @@ import { SharedModule } from '../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NgOptimizedImage } from '@angular/common';
+import { TranslateModule } from "@ngx-translate/core";
 
 import { FeaturesSupportedComponent } from './components/features-supported/features-supported.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { FinalCtaComponent } from './components/final-cta/final-cta.component';
-import { CallToActionButtonComponent } from './components/call-to-action-button/testimonials/call-to-action-button.component';
-import { TranslateModule } from "@ngx-translate/core";
+import {
+  CallToActionButtonComponent
+} from './components/call-to-action-button/testimonials/call-to-action-button.component';
+import { CoverLetterExamplesComponent } from "./components/cover-letter-examples/cover-letter-examples.component";
 
 @NgModule({
   declarations: [
@@ -17,9 +20,11 @@ import { TranslateModule } from "@ngx-translate/core";
     TestimonialsComponent,
     FinalCtaComponent,
     CallToActionButtonComponent,
+    CoverLetterExamplesComponent
   ],
   imports: [HomeRoutingModule, SharedModule, NgOptimizedImage, TranslateModule],
   exports: [],
   providers: [],
 })
-export class HomeModule {}
+export class HomeModule {
+}
